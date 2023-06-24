6a6d4efc563cf1c164218dc168f506f01cbbfe5b
Android Dev <javakotlin69@gmail.com>
2023-06-24 19:38:42 +0530

    Full Setup Retrofit, LiveData, Coroutine

--- app/src/main/java/com/example/android_base/MainActivity.kt
+++ app/src/main/java/com/example/android_base/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.android_base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.android_base.retrofit.ResponseStatus
+import com.example.android_base.utils.show
+import com.example.android_base.utils.toast
+import com.example.android_base.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val _postViewModel by viewModels<PostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _postViewModel.getPostVM(this)
+        observer()
+    }
+
+    private fun observer() {
+        val loader = show()
+        _postViewModel.post.observe(this) { post ->
+            post.getContentIfNotHandled().let { it ->
+                when (it) {
+                    is ResponseStatus.Loading -> {}
+                    is ResponseStatus.Success -> {
+                        loader.dismiss()
+                    }
+
+                    is ResponseStatus.Error -> {
+                        loader.dismiss()
+                        it.message.toString().toast(this)
+                    }
+
+                    else -> {}
+                }
+            }
+        }
     }
 }
